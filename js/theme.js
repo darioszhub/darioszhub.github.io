8956b3a9f4bb575d2eee812ede7567c1d6bde42e
Dario Sebastiano Grillo <dariog40@gmail.com>
2026-08-19 16:34:11 +0200

    fix: use dark theme as default

--- js/theme.js
+++ js/theme.js
@@ -1,8 +1,8 @@
-//load theme preference first
+// Load theme preference first
 const savedTheme = localStorage.getItem("theme");
 
 if (savedTheme) {
     document.documentElement.setAttribute("data-theme", savedTheme);
-} else if (window.matchMedia("(prefers-color-scheme: light)").matches) {
-    document.documentElement.setAttribute("data-theme", "light");
+} else {
+    document.documentElement.setAttribute("data-theme", "dark");
 }
